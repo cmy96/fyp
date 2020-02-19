044e275c76220d795848209c4d12134c5de51665
jiayuntan <34365725+jiayuntan@users.noreply.github.com>
2020-02-19 11:57:13 +0800

    changes to webpage

--- Frontend/Code/fyp2/application/dash_application/dash_example.py
+++ Frontend/Code/fyp2/application/dash_application/dash_example.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import dash
 import dash_table
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import pandas as pd
 from .layout import html_layout
 import json
@@ -14,21 +15,135 @@ from dash.dependencies import Input, Output
 from textwrap import dedent as d
 import numpy as np
 import operator
+from plotly.subplots import make_subplots
+import plotly
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 #df = pd.read_csv("data/clinical.csv")
 
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
+#app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Link", href="#")),
+        dbc.DropdownMenu(
+            nav=True,
+            in_navbar=True,
+            label="Menu",
+            children=[
+                dbc.DropdownMenuItem("Entry 1"),
+                dbc.DropdownMenuItem("Entry 2"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Entry 3"),
+            ],
+        ),
+    ],
+    brand="Demo",
+    brand_href="#",
+    sticky="top",
+)
+
+labels = ["Survived", "Dead"]
+values = [0.7, 0.3]
+
+trace1 = go.Bar(
+    x=["-6 Months", "Year 0", "Year 1", "Year 2", " Year 5", "Year 10"],
+    y=[100,200,300,400,500,600],
+    name='Surgery'
+)
+
+trace2= go.Bar(
+    x=["-6 Months", "Year 0", "Year 1", "Year 2", " Year 5", "Year 10"],
+    y=[50, 60, 70, 80, 90, 100],
+    name='Medicine'
+)
+
+
+body = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H2("Heading"),
+                        html.P(
+                            """\
+Donec id elit non mi porta gravida at eget metus.
+Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non
+mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed
+odio dui."""
+                        ),
+                        dbc.Button("View details", color="secondary"),
+                    ],
+                    md=4,
+                ),
+                dbc.Col(
+                    [
+                        html.H2("Graph"),
+                        dcc.Graph(
+                            figure={"data": [{"x": [1, 2, 3], "y": [1, 4, 9]}]}
+                        ),
+                    ]
+                ),
+            ]
+        ),
+        dbc.Row(
+            dbc.Col(
+                [
+                    dcc.Graph(
+                        id="suvivability",
+                        figure=go.Figure(
+                            data=[
+                                go.Pie(
+                                    labels=labels, values=values, hole=.3
+                                    )
+                                ],
+                            layout=go.Layout(
+                            title="Pie Chart"
+                            )
+                        )
+                    )
+                ],
+            ),
+
+        ),
+        dbc.Row(
+            dbc.Col(
+                [
+                    dcc.Graph(
+                        id="Cost Prediction",
+                        figure=go.Figure(
+                            data=[trace1, trace2],
+                            
+                            layout=go.Layout(
+                                barmode='stack',
+                                title='Stacked Bar Chart'
+                            )
+                        ),
+                    )
+                ]
+            )
+        )
+    ],
+    className="mt-4",
+)
+
 
 
 def Add_Dash(server):
     """Create a Dash app."""
-    external_stylesheets = ['/static/dist/css/styles.css',
+    external_stylesheets = ['C:\wamp64\www\\fyp2\\application\static\dist\css\style.css',
                             'https://fonts.googleapis.com/css?family=Lato',
-                            'https://use.fontawesome.com/releases/v5.8.1/css/all.css']
+                            'https://use.fontawesome.com/releases/v5.8.1/css/all.css', 
+                            dbc.themes.BOOTSTRAP]
     external_scripts = ['/static/dist/js/includes/jquery.min.js',
                         '/static/dist/js/main.js']
     dash_app = dash.Dash(server=server,
@@ -37,42 +152,51 @@ def Add_Dash(server):
                          routes_pathname_prefix='/dashapp/')
 
     # Override the underlying HTML template
-    #dash_app.index_string = html_layout
+    dash_app.index_string = html_layout
 
     # Create Dash Layout comprised of Data Tables
     dash_app.layout = html.Div(
-
-        #children=get_datasets(),
-        #id='dash-container',
-
         children = [
-
-            html.Div(
-                html.Nav(className = "nav nav-pills", children = [
-                html.A('C.A.R.E', className="nav-item nav-link btn", href='/'),
-                html.A('Calculator', className="nav-item nav-link active btn", href='/dashapp') 
-                ]
-                )
-            ),
-            html.H1("lalallaala"),
-            html.H1("lallalalalala"),
-
-            html.Div(
-                dcc.Graph(
-                    id='example-graph',
-                    figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                    ],
-                    'layout': {
-                    'title': 'Dash Data Visualization'
-                    }
-                    }
-                )
-            )
-        ]
+        body
+        ], style={'color': 'red', 'fontSize': 14}
+        
     )
+    
+        
+    #     html.Div(
+
+    #     #children=get_datasets(),
+    #     #id='dash-container',
+
+    #     children = [
+
+    #         html.Div(
+    #             html.Nav(className = "nav nav-pills", children = [
+    #             html.A('C.A.R.E', className="nav-item nav-link btn", href='/'),
+    #             html.A('Calculator', className="nav-item nav-link active btn", href='/dashapp') 
+    #             ]
+    #             )
+    #         ),
+    #         html.Div(
+    #             html.H1("Dashboard"),
+    #         ),
+
+    #         html.Div(
+    #             dcc.Graph(
+    #                 id='example-graph',
+    #                 figure={
+    #                 'data': [
+    #                     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+    #                     {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+    #                 ],
+    #                 'layout': {
+    #                 'title': 'Dash Data Visualization'
+    #                 }
+    #                 }
+    #             )
+    #         )
+    #     ]
+    # )
 
     return dash_app.server
 
--- Frontend/Code/fyp2/application/dash_application/layout.py
+++ Frontend/Code/fyp2/application/dash_application/layout.py
@@ -6,12 +6,28 @@ html_layout = '''<!DOCTYPE html>
                             {%favicon%}
                             {%css%}
                         </head>
+
                         <body>
-                            <nav>
-                              <a href="/"><i class="fas fa-home"></i> Home</a>
-                              <a href="/dashapp/"><i class="fas fa-chart-line"></i> Embdedded Plotly Dash</a>
+                            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <a class="navbar-brand" href="#"><img src="C:\wamp64\www\\fyp2\\application\\templates\Logo.png"> C.A.R.E</a>
+                              <span class="navbar-text navbar-text-color">Breast Cancer Prediction Tool</span>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+    
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/"><i class="fas fa-calculator"></i> Calculator </a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/dashapp"><i class="fas fa-chart-pie"></i> Dashboard <span class="sr-only">(current)</span></a>
+                                </li>
+          
+                            </div>
                             </nav>
                             {%app_entry%}
+
                             <footer>
                                 {%config%}
                                 {%scripts%}
